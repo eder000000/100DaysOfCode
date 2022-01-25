05296ce8036d248d15e0c2f1e2a2e39b288dc201
Eder Garcia <eder000000@gmail.com>
2022-01-24 23:49:32 -0600

    Day 004 code complete

---
+++ day004/CountUniqueValues.java
@@ -0,0 +1,36 @@
+//Implement a function called countUniqueValues,
+//which accepts a sorted array, and counts the
+//unique values in the array. There can be
+//negative numbers in the array, but it will always
+//be sorted.
+
+
+//O(n)
+package day004;
+
+import java.util.Arrays;
+
+public class CountUniqueValues {
+
+	static int[] arrayGiven = {-2, -1, -1, 0, 1 };
+	static int numOfUniqueValues = 0;
+	
+	public static void main(String... args) {
+		System.out.println(Arrays.toString(arrayGiven));
+		System.out.println(countUniqueValues(arrayGiven));
+	}
+	
+	public static int countUniqueValues(int[] array) {
+		if(array.length == 0) {
+			return numOfUniqueValues;
+		}
+		//Multiple pointers
+		for(int i = 1; i < array.length; i++) {
+			if(array[numOfUniqueValues] != array[i]) {
+				numOfUniqueValues++;
+				array[numOfUniqueValues] = array[i];
+			}
+		}
+		return numOfUniqueValues + 1;
+	}
+}
