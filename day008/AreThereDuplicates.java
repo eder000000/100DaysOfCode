974f372c9e971d8df42e34b931e249b07e0ea820
Eder Garcia <eder000000@gmail.com>
2022-02-03 10:15:57 -0600

    day008 AreThereDuplicates code start

---
+++ day008/AreThereDuplicates.java
@@ -0,0 +1,9 @@
+//Implement a function code which accepts a variable number of arguments
+//and checks  there are any duplicates among tha arguments passed in.
+
+
+package day008;
+
+public class AreThereDuplicates {
+
+}
