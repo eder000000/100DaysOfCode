1a56fa1047b6dc780a07cbebdd9f0d036ede338d
Eder Garcia <eder000000@gmail.com>
2022-02-03 19:56:48 -0600

    First version of ReversedSequence code done.

---
+++ day009/ReversedSequence.java
@@ -0,0 +1,38 @@
+//Build a function that returns an array of integers from n to 1 where n>0.
+package day009;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ReversedSequence {
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Enter integer number ");
+		int num = scan.nextInt();
+		try {
+			if(num < 1) {
+				 throw new Exception("Invalid input");
+			}
+			System.out.println(num);
+			System.out.println(Arrays.toString(reverse(num)));
+			
+		}
+			catch (Exception e){
+				System.out.println(" Number must be greather than 0.");
+			} 
+	}
+	
+	public static int[] reverse(int num) {
+		int[] array = new int[num];
+		int counter = 0;
+		
+		for(int i = num - 1; i >= 0; i--) {
+			array[counter] = num;
+			counter++;
+			num--;
+		}
+		return array;
+	}
+}
