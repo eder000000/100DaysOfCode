17ca7b0b8c6136c90ce140aee4c360f307365a65
Eder Garcia <eder000000@gmail.com>
2022-02-08 10:24:25 -0600

    Code completed.

--- day008/AreThereDuplicates.java
+++ day008/AreThereDuplicates.java
@@ -1,4 +1,4 @@
-//Implement a function code which accepts a variable number of arguments
+//Implement a function code which accepts a variable number of integer arguments
 //and checks  there are any duplicates among the arguments passed in.
 
 
@@ -6,4 +6,20 @@ package day008;
 
 public class AreThereDuplicates {
 
+	public static void main(String[] args) {
+
+		System.out.println(areThereDuplicates(3, 3, 1, 0, 9));
+	
+	}
+	
+	public static boolean areThereDuplicates(Integer... args) {		
+		for(int i = 0; i <= args.length; i++) {
+			int pointer = 1;
+			if(args[i] == args[pointer]) {
+				return true;
+			}
+			pointer++;
+		}
+		return false;
+	}
 }
